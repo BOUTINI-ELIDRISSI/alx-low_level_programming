392914b1b7b1d39a9743f21393074e1a3bd5efc9
ikram <ikram.boutinielidrissi@gmail.com>
2023-03-21 12:39:09 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Print the add of two numbers
+ *
+ * @a: input number as an integer
+ * @b: input number as an integer
+ *
+ * Return: number
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
