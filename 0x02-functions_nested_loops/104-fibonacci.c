2a3908fe59899fb9990ac01a40c812cedf0e5c90
ikram <ikram.boutinielidrissi@gmail.com>
2023-03-21 15:14:28 -0700

    My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half, fib1_half2, fib2_half1, fib2_half2;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
 	for (count = 0; count < 92; count++)
@@ -40,6 +40,6 @@ int main(void)
 		fib2_half1 = half1;
 		fib2_half2 = half2;
 	}
-	print("\n");
+	printf("\n");
 	return (0);
 }
