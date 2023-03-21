35fb6879096685b6bbbaeaa5791f807904422c23
ikram <ikram.boutinielidrissi@gmail.com>
2023-03-21 13:36:56 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Show numbers to 98
+ *
+ * @n: input number as an integer
+ *
+ * Return: no return
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else if (n == 98)
+	{
+		printf("%d", n);
+	}
+	else if (n >= 98)
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+}
