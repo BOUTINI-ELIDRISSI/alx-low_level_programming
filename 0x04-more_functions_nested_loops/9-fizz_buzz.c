b3b3dbf233684c05d71bbfbfb89a573085f3ee46
ikram <ikram.boutinielidrissi@gmail.com>
2023-03-23 11:41:18 -0700

    You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,22 +6,22 @@
  *
  * Return: Always 0.
  */
-int main (void)
+int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if((i % 3 == 0) && (i % 5 != 0))
+		if ((i % 3 == 0) && (i % 5 != 0))
 			printf("Fizz");
 		else if ((i % 5 == 0) && (i % 3 != 0))
 			printf("Buzz");
-    		else if ((i % 3 == 0) && (i % 5 == 0))
+		else if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz");
-    		else
-       			printf("%d", i);
-    		if(i != 100)
+		else
+			printf("%d", i);
+		if (i != 100)
 			printf(" ");
-        }
+	}
 	printf("\n");
 }
