67b7e6ed2366fb60b8a8866905eabe7abd9b84e0
ikram <ikram.boutinielidrissi@gmail.com>
2023-03-23 08:23:42 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - Shows 1 if the input is a
+ * uppercase character. Another cases, shows
+ * 0
+ *
+ * @c: The input number ad an integer
+ *
+ * Return: 1 for uppercase character. 0 for the rest.
+ */
+int _isupper(int c)
+{
+	if (c >= 41 && c <= 90)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
