1697dbbd3a53adbcd6d4904a9f1c4eac98bcfe58
ikram <ikram.boutinielidrissi@gmail.com>
2023-04-19 14:02:56 -0700

    TASK. 3

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int bytes i;
+	int bytes, i;
 	char *arr;
 
 	if (argc != 2)
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 	arr = (char *)main;
 	for (i = 0; i < bytes; i++)
 	{
-		if (i == bytes -1)
+		if (i == bytes - 1)
 		{
 			printf("%02hhx\n", arr[i]);
 			break;
