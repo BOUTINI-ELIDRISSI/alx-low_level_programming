1d14cd43f30651e95152ed271aafe75039dd4ae2
ikram <ikram.boutinielidrissi@gmail.com>
2023-04-19 13:10:20 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -29,7 +29,9 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*op == '/' && num2 == 0) || (*op == ' % ' && num2 == 0))
+	if ((*op == '/' && num2 == 0) ||
+			(*op == '%' && num2 == 0))
+
 	{
 		printf("Eroor\n");
 		exit(100);
