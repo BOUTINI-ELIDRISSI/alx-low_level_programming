dd628ddfab8204ba83a547581be01fe1e772e335
ikram <ikram.boutinielidrissi@gmail.com>
2023-05-02 10:21:46 -0700

    Task.0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints list.h
+ * @h: linked list
+ *
+ * Return: num of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nemuro = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->a);
+		nemuro++;
+		h = h->next;
+	}
+	return (nemuro);
+}
