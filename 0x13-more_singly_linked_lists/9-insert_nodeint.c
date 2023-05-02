b44c90640111667215fb48ec2a33921ea2b92412
ikram <ikram.boutinielidrissi@gmail.com>
2023-05-02 11:25:33 -0700

    Task.9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (rakam == idx - 1)
 		{
-			rakam->next = temp->next;
+			yene->next = chigan->next;
 			chigan->next = yene;
 			return (yene);
 		}
